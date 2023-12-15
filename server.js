fcfa8a89494bef88c8a7262c1bbc2f1f28b47052
Анастасия Колмакова <nastya.miss.7@gmail.com>
2023-12-15 20:39:52 +0400

    adding ID column

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const userSchema = new mongoose.Schema({
 
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/task4', { useNewUrlParser: true, useUnifiedTopology: true });
 const User = mongoose.model('User', userSchema);
 module.exports = User;
 
